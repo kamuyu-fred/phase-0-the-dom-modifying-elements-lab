760c952c0550885813b46b3585d6ace69d8e793b
fred <daktarispacetime@gmail.com>
2022-11-22 08:45:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const removingMain = document.getElementById('main')
+console.log(removingMain)
+removingMain.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Fred is the champion"
+document.body.append(newHeader)
